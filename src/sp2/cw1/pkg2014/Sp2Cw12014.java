faeabe70a60fba8c3746a5a602d035015cf2dc22
whelptp <whelptp@CZC4253G3T.richmond.ac.uk>
2014-10-06 13:36:56 +0100

    Initial commit of new project

---
+++ src/sp2/cw1/pkg2014/Sp2Cw12014.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp2.cw1.pkg2014;
+
+/**
+ *
+ * @author whelptp
+ */
+public class Sp2Cw12014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
